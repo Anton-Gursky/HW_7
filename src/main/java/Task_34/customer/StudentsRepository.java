c5f6e8fa1e93b742a0bf150bef7321dda20c33d2
Anton Gursky <a.gursky@gmail.com>
2023-05-06 23:48:53 +0300

    Task_34 added

---
+++ src/main/java/Task_34/customer/StudentsRepository.java
@@ -0,0 +1,16 @@
+package Task_34.customer;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface StudentsRepository extends CrudRepository<Students, Long> {
+
+    @Query(value = "SELECT c FROM Students c WHERE c.name LIKE '%' || :keyword || '%'"
+            + " OR c.email LIKE '%' || :keyword || '%'"
+            + " OR c.address LIKE '%' || :keyword || '%'")
+    public List<Students> search(@Param("keyword") String keyword);
+}
